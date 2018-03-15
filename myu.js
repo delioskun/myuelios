35183d1d74dfb03fdd72089db8baa0f616c8a397
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-14 21:38:45 -0300

    Update myu.js

--- myu.js
+++ myu.js
@@ -111,7 +111,7 @@ myu.on('message', message => {
 		 break;
 		 case 'reportchannel':
 		 let channel = args[0];
-		 console.log('teste');
+		 console.log(message.author.hasPermission("ADMINISTRATOR"));
 		if(message.guild.owner.hasPermission("ADMINISTRATOR")){ 
 		 if(myu.channels.exists("name", channel)){
 		 

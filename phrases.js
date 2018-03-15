ef3dcf397735841b907d465d5c450145a6313124
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-15 16:18:55 -0300

    Create phrases.js

---
+++ phrases.js
@@ -0,0 +1,30 @@
+module.exports = {
+   myu_phrases: function() {
+      return [
+		 "Elesis. O jogo de ação do momento!",
+		 "Lorde ficou engraçado agora: KEKEKEKEKEKE",
+		 "Alguém disse, sacrossanto?",
+		 "Brilha, brilha, espadinha. Quero ver você julgar!",
+		 "Eve rainha, resto nadinha",
+		 "Eu já disse que sou uma bot de familia. Não vou fazer piadas com a Código Absoluto D:",
+		 "Meus sensores estão com pouca memória Ran, acho que a Ara passou por aqui -w-",
+		 "As andorinhas voltaram! E a Vishnum também!",
+		 "O nome do jogo é Elsword? Juro que nem reparei ;-;",
+		 "Keep Calm e ESPADA ANCESTRAL!",
+		 "Rosas são vermelhas, violetas são azuis. DFO é praquele lado, pelo amor de Jesus!",
+		 "Minerva! Limpeza, maciez e perfume de Rosas.",
+		 "Hayaaaa!!!!",
+		 "Eu quero levar um Angkor pra casa <3",
+		 "Todo natal a Rena some -.-'",
+		 "WS tem bom gosto, por isso escolheu o Ventus qq",
+		 "Neste momento uma conferência de Lolis ocorre na sala Luciel",
+		 "Quem joga com capricho. Joga com Primor!",
+		 "Quando que o Raven vem me substituir? -.-'",
+		 "Acho que o BM ficou muito Furioso D:",
+		 "Pi....Ka....Chunnnng!!!!",
+		 "Este último look do Templário Cósmico ficou um ar-ra-so!",
+		 "O DDD pra Add o Add...3ioo38282j83c398c239n. **Fatal Error: Error on NaN Class.** ",
+		 "Há quanto tempo não vejo o Esper diabólico!Tá, essa foi péssima :C"
+		 ];
+   }
+}

4172e748deac081254385de23cb91e350847b345
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-16 18:28:20 -0300

    Update jsearch.js

--- jsearch.js
+++ jsearch.js
@@ -2,91 +2,6 @@ var http 	= require("http"),
 	https 	= require("https")
 
 var	urlRegExp = /(http|https)(\:\/\/)((www\.|)elwiki.net|sites)(.*?)(?=\")/gi
-	
-exports.google = function (gQuery, gPage, gCb){
-	
-	if( arguments.length===3 ){ 
-		
-		if( typeof(arguments[1])=="number" && typeof(arguments[2])=="function" ){
-			
-			// google search can be start :)
-			gGelen = ""
-			
-			for(g=0;g<=arguments[1];g++){		
-				
-				gAyarlar = {
-					host : "www.google.com.tr",
-					path : "/search?q="+arguments[0]+"&start="+g+"&sa=N&gws_rd=ssl",
-				}
-				
-				https.request(gAyarlar,(res)=>{
-					
-					res.on("data",(d)=>{
-						gGelen += d;
-					})
-					
-					res.on("end",()=>{
-						var gSon = gGelen.match(urlRegExp)
-						gCb(gSon)
-					})
-					
-				}).end();
-				
-			}
-				
-		}else{
-			gCb("Argument type error!")
-		}
-		
-	}else{
-		gCb("Function argument missed!")
-	}
-	
-}
-
-exports.yandex = function ( yQuery, yPage, yCb ){
-	
-	if( arguments.length===3 ){ 
-		
-		if( typeof(arguments[1])=="number" && typeof(arguments[2])=="function" ){
-			
-			// yandex search can be start :)
-			yGelen = ""
-			
-			for(y=0;y<=arguments[1];y++){
-				
-				yAyarlar = {
-					host : "www.yandex.com.tr",
-					path : "/search/?lr=101515&text="+arguments[0]+"&p="+y
-					
-				}
-				
-				https.request(yAyarlar,(res)=>{
-					
-					res.on("data",(d)=>{
-						yGelen += d;
-					})
-					
-					res.on("end",()=>{
-						
-						var ySon = yGelen.match(urlRegExp)
-						yCb(ySon)
-						
-					})
-					
-				}).end();
-				
-			}
-			
-		}else{
-			yCb("Argument type error!")
-		}
-		
-	}else{
-		yCb("Function argument missed!")
-	}
-	
-}
 
 exports.bing = function ( bQuery, bPage, bCb ){
 	
@@ -107,6 +22,7 @@ exports.bing = function ( bQuery, bPage, bCb ){
 						bGelen += d;
 					})
 					res.on("end",()=>{
+						console.log(bGelen);
 						var bSon = bGelen.match(urlRegExp)
 						bCb(bSon)
 					})

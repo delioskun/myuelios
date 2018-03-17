b0979da2ca2a2c343e416af2ce43aaf629f4f8cd
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-17 14:43:43 -0300

    Update jsearch.js

--- jsearch.js
+++ jsearch.js
@@ -5,7 +5,7 @@ exports.elwiki = function ( bQuery, bPage, bCb ){
 	if( arguments.length===3 ){ 	
 		if( typeof(arguments[1])=="number" && typeof(arguments[2])=="function" ){
 			request('https:elwiki.net', function (error, response, html) {
-			process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0	
+			process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 			console.log(error);
   if (!error && response.statusCode == 200) {
     console.log(html);

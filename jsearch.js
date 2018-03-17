3a6142a0758564ab5766488be76609f8df6ea7e0
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-17 13:44:45 -0300

    Update jsearch.js

--- jsearch.js
+++ jsearch.js
@@ -4,7 +4,7 @@ var cheerio = require('cheerio');
 exports.elwiki = function ( bQuery, bPage, bCb ){
 	if( arguments.length===3 ){ 	
 		if( typeof(arguments[1])=="number" && typeof(arguments[2])=="function" ){
-			request('https://www.google.com', function (error, response, html) {
+			request('https://elwiki.net/w/Main_Page', function (error, response, html) {
   if (!error && response.statusCode == 200) {
     console.log(html);
   }

c224a39bd1f82fc639b6bd7411c7532575b739c3
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-17 14:43:25 -0300

    Update jsearch.js

--- jsearch.js
+++ jsearch.js
@@ -4,7 +4,8 @@ var cheerio = require('cheerio');
 exports.elwiki = function ( bQuery, bPage, bCb ){
 	if( arguments.length===3 ){ 	
 		if( typeof(arguments[1])=="number" && typeof(arguments[2])=="function" ){
-			request('https://br.search.yahoo.com/search;_ylt=A2KLfRbLUa1a0hYAgxry6Qt.;_ylc=X1MDMjExNDcxMDAwMgRfcgMyBGZyAwRncHJpZANDYmxKeXRmaFFUdVFrRzN0SXJ4UVRBBG5fcnNsdAMwBG5fc3VnZwMwBG9yaWdpbgNici5zZWFyY2gueWFob28uY29tBHBvcwMwBHBxc3RyAwRwcXN0cmwDBHFzdHJsAzE2BHF1ZXJ5A1Jvc2UlMjBlbCUyMHdpa2kEdF9zdG1wAzE1MjEzMDgxMTc-?fr2=sb-top-br.search&p=Elesis+el+wiki&fr=sfp&iscqry=', function (error, response, html) {
+			request('https:elwiki.net', function (error, response, html) {
+			process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0	
 			console.log(error);
   if (!error && response.statusCode == 200) {
     console.log(html);

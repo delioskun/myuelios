dbefebdd854a2a24b5043faa4419b6ec035a545f
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-17 13:29:47 -0300

    Update jsearch.js

--- jsearch.js
+++ jsearch.js
@@ -4,7 +4,7 @@ exports.elwiki = function ( bQuery, bPage, bCb ){
 	if( arguments.length===3 ){ 	
 		if( typeof(arguments[1])=="number" && typeof(arguments[2])=="function" ){
 			const $ = cheerio.load('https://elwiki.net/wiki/index.php?search=Sariel&title=Special%3ASearch&go=Go');
-			console.log($);
+			console.log($('li'));
 		}else{
 			bCb("Argument type error!");
 		}

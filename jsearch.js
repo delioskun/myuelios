fa9325fa64bb6e6ea6ad3a8731ed8fef431ec266
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-17 13:11:48 -0300

    Update jsearch.js

--- jsearch.js
+++ jsearch.js
@@ -1,49 +1,15 @@
-var https 	= require("https");
-
-exports.bing = function ( bQuery, bPage, bCb ){
-	
-	if( arguments.length===3 ){ 
-		
-		if( typeof(arguments[1])=="number" && typeof(arguments[2])=="function" ){
-			
-			// bing search can be start :)
-			var bGelen = ""; 
-				bAyarlar = { host: "www.bing.com", path: "/search?q="+arguments[0] }
-				https.request(bAyarlar,(res)=>{ res.on("data",(d)=>{ bGelen += d; })
-					console.log(bGelen);		       
-					res.on("end",()=>{ console.log("Gotcha!"); bCb(bGelen.match(/(https|http)\:\/\/(www.|)(.*?)(?=\")/g)); })
-				}).end()
-							
-		}else{
-			bCb("Argument type error!");
-		}
-		
-	}else{
-		bCb("Function argument missed!");
-	}
-	
-}
+getSource = require ('get-source');
 
 exports.elwiki = function ( bQuery, bPage, bCb ){
-	
-	if( arguments.length===3 ){ 
-		
+	if( arguments.length===3 ){ 	
 		if( typeof(arguments[1])=="number" && typeof(arguments[2])=="function" ){
-			
-			// bing search can be start :)
-			var bGelen = ""; 
-				bAyarlar = { host: "elwiki.net", path: "/index.php?search=" + arguments[0] + "&title=Special%3ASearch&go=Go" }
-				https.request(bAyarlar,(res)=>{ res.on("data",(d)=>{ bGelen += d; })		       
-					res.on("end",()=>{ console.log("Gotcha!"); bCb(bGelen.match(/(https|http)\:\/\/(www.|)(.*?)(?=\")/g)); })
-				}).end()
-							
+			file = getSource ('www.google.com');
+			console.log(file);
 		}else{
 			bCb("Argument type error!");
 		}
-		
 	}else{
 		bCb("Function argument missed!");
 	}
-	
 }
 

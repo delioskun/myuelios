69066d1ee21f1530e16433fef63380f91632427a
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-17 13:37:52 -0300

    Update jsearch.js

--- jsearch.js
+++ jsearch.js
@@ -5,9 +5,7 @@ exports.elwiki = function ( bQuery, bPage, bCb ){
 	if( arguments.length===3 ){ 	
 		if( typeof(arguments[1])=="number" && typeof(arguments[2])=="function" ){
 			request('https://www.elwiki.net/wiki/index.php?search=Sariel&title=Special%3ASearch&go=Go', function (error, response, html) {
- 			 if (!error && response.statusCode == 200) {
-    			console.log("teste");
-  			}
+ 			 console.log(error);
 });
 		}else{
 			bCb("Argument type error!");

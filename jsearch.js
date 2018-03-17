1c624abe51bcda326a3ec2f78f2fb9d20cebe6f3
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-17 14:25:50 -0300

    Update jsearch.js

--- jsearch.js
+++ jsearch.js
@@ -5,6 +5,7 @@ exports.elwiki = function ( bQuery, bPage, bCb ){
 	if( arguments.length===3 ){ 	
 		if( typeof(arguments[1])=="number" && typeof(arguments[2])=="function" ){
 			request('https://www.elwiki.net/index.php', function (error, response, html) {
+			console.log(error);
   if (!error && response.statusCode == 200) {
     console.log(html);
   }

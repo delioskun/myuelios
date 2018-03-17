0ed1b6afc324c0dd3bf5265c2453d6bd16be1af8
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-17 13:43:25 -0300

    Update jsearch.js

--- jsearch.js
+++ jsearch.js
@@ -4,7 +4,7 @@ var cheerio = require('cheerio');
 exports.elwiki = function ( bQuery, bPage, bCb ){
 	if( arguments.length===3 ){ 	
 		if( typeof(arguments[1])=="number" && typeof(arguments[2])=="function" ){
-			request('https://news.ycombinator.com', function (error, response, html) {
+			request('https://www.google.com', function (error, response, html) {
   if (!error && response.statusCode == 200) {
     console.log(html);
   }

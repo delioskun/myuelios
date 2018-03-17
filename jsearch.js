4affaf078dfa65bd536c9d4f9fb75eb3e9f80f47
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-17 15:23:02 -0300

    Update jsearch.js

--- jsearch.js
+++ jsearch.js
@@ -7,7 +7,7 @@ exports.elwiki = function ( bQuery, bPage, bCb ){
 			request('https://www.bing.com/search?q=' + arguments[0], function (error, response, html) {
 			process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 			console.log(error);
- 		 if (!error && response.statusCode == 200) { bCb(html.match('(http|https)\:\/\/elwiki.net\/(.*?)(?=")/gi')); }
+ 		 if (!error && response.statusCode == 200) { bCb(html.match(/http(s|)\:\/\/(www.|)(\<strong\>|)elwiki\.net\/(.*?)(?=\")/gi)); }
 	});
 		}else{
 			bCb("Argument type error!");
@@ -16,4 +16,22 @@ exports.elwiki = function ( bQuery, bPage, bCb ){
 		bCb("Function argument missed!");
 	}
 }
+
+exports.forum = function ( bQuery, bPage, bCb ){
+	if( arguments.length===3 ){ 	
+		if( typeof(arguments[1])=="number" && typeof(arguments[2])=="function" ){
+			request('https://www.bing.com/search?q=' + arguments[0], function (error, response, html) {
+			process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+			console.log(error);
+ 		 if (!error && response.statusCode == 200) { bCb(html.match(/http(s|)\:\/\/(www.|)(\<strong\>|)sites\.levelupgames\.com\.br\/(.*?)(?=\")/gi)); }
+	});
+		}else{
+			bCb("Argument type error!");
+		}
+	}else{
+		bCb("Function argument missed!");
+	}
+}
+
+
 

e2fe3ecf8c7e86cbd4a045c36f8ad1f57d5c3694
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-17 13:35:25 -0300

    Update jsearch.js

--- jsearch.js
+++ jsearch.js
@@ -1,10 +1,14 @@
+var request = require('request');
 var cheerio = require('cheerio');
 
 exports.elwiki = function ( bQuery, bPage, bCb ){
 	if( arguments.length===3 ){ 	
 		if( typeof(arguments[1])=="number" && typeof(arguments[2])=="function" ){
-			const $ = cheerio.load('https://elwiki.net/wiki/index.php?search=Sariel&title=Special%3ASearch&go=Go');
-			console.log($('li'));
+			request('https://elwiki.net/wiki/index.php?search=Sariel&title=Special%3ASearch&go=Go', function (error, response, html) {
+ 			 if (!error && response.statusCode == 200) {
+    			console.log(html);
+  			}
+});
 		}else{
 			bCb("Argument type error!");
 		}

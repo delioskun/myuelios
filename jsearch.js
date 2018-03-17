a109702ad82da29c0027b8edd2036e1508140a10
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-17 12:52:18 -0300

    Update jsearch.js

--- jsearch.js
+++ jsearch.js
@@ -24,3 +24,26 @@ exports.bing = function ( bQuery, bPage, bCb ){
 	
 }
 
+exports.elwiki = function ( bQuery, bPage, bCb ){
+	
+	if( arguments.length===3 ){ 
+		
+		if( typeof(arguments[1])=="number" && typeof(arguments[2])=="function" ){
+			
+			// bing search can be start :)
+			var bGelen = ""; 
+				bAyarlar = { host: "elwiki.net", path: "/index.php?search=" + arguments[0] + "&title=Special%3ASearch&go=Go" }
+				https.request(bAyarlar,(res)=>{ res.on("data",(d)=>{ bGelen += d; })		       
+					res.on("end",()=>{ console.log("Gotcha!"); bCb(bGelen.match(/(https|http)\:\/\/(www.|)(.*?)(?=\")/g)); })
+				}).end()
+							
+		}else{
+			bCb("Argument type error!");
+		}
+		
+	}else{
+		bCb("Function argument missed!");
+	}
+	
+}
+

ea0f7002f64a0b5a977464fa9d81176a32878f5e
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-18 14:02:19 -0300

    Update jsearch.js

--- jsearch.js
+++ jsearch.js
@@ -14,9 +14,9 @@ exports.elwiki = function ( bQuery, bPage, bCb ){
 exports.forum = function ( bQuery, bPage, bCb ){
 	if( arguments.length!=3 ){bCb("Function argument missed!");return;} 	
 	if( typeof(arguments[1])!="number" && typeof(arguments[2])!="function" ){bCb("Argument type error!");return;}
+	console.log(bQuery);
 	var url = String('http://sites.levelupgames.com.br/forum/elsword/search.php?do=process&query=' + bQuery + '&titleonly=1');
 	request(url, function (error, response, body) {
-  	console.log(response.request.uri.href);
 	request(response.request.uri.href, function (a, b, c) {
 	console.log(c.match(/showthread(.*?)(?=\;s)/gi));	
 	});

eb1106e1aba12bfc6135b38d74a382f4788c4e91
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-18 13:43:01 -0300

    Update jsearch.js

--- jsearch.js
+++ jsearch.js
@@ -20,10 +20,11 @@ exports.elwiki = function ( bQuery, bPage, bCb ){
 exports.forum = function ( bQuery, bPage, bCb ){
 	if( arguments.length!=3 ){bCb("Function argument missed!");return;} 	
 	if( typeof(arguments[1])!="number" && typeof(arguments[2])!="function" ){bCb("Argument type error!");return;}
-	request('http://sites.levelupgames.com.br/forum/elsword/search.php?do=process&query=Black+Massacre&titleonly=1', function (error, response, body) {
+	var url = 'http://sites.levelupgames.com.br/forum/elsword/search.php?do=process&query=' + bQuery + '&titleonly=1';
+	request(url, function (error, response, body) {
   	console.log(response.request.uri.href);
 	request(response.request.uri.href, function (a, b, c) {
-	console.log(c.match(/showthread(.*?)(?=\")/gi));	
+	console.log(c.match(/showthread(.*?)(?=\;s)/gi));	
 	});
 	});	
 }

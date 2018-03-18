0dcf505c1529df5f001bfaefac0dbc8c7a6ea1d7
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-18 15:09:32 -0300

    Update myu.js

--- myu.js
+++ myu.js
@@ -127,6 +127,7 @@ myu.on('message', message => {
 			var r_f = false;
 			if(usersearch){ 
 			js.forum(encodeURIComponent(usersearch),1,function(response){	
+			console.log(response);	
 			if (!r_f && response != null){ var result_tab = response.filter(function(n){return n.match(/elsword/g)})[0]}; r_f = true;
 			if(result_tab != undefined){
 				result_tab = "http://sites.levelupgames.com.br/forum/elsword/" + result_tab;

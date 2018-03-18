4f06a13417a54d16cab619f4e682e5bf0ee3b0fc
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-18 04:33:30 -0300

    Update jsearch.js

--- jsearch.js
+++ jsearch.js
@@ -23,7 +23,7 @@ exports.forum = function ( bQuery, bPage, bCb ){
 		request('http://sites.levelupgames.com.br/forum/elsword/search.php?do=process&query=' + arguments[0] + '&titleonly=1', function (error, response, html) {
 			process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 			console.log(html);
- 		 if (!error && response.statusCode == 200) { bCb('teste')); }
+ 		 if (!error && response.statusCode == 200) { bCb('teste'); }
 	});		
 		}else{
 			bCb("Argument type error!");

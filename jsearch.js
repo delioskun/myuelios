abc52124850db5472c2520694c92a11a2542a5c6
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-18 16:07:25 -0300

    Update jsearch.js

--- jsearch.js
+++ jsearch.js
@@ -4,10 +4,12 @@ var cheerio = require('cheerio');
 exports.elwiki = function ( bQuery, bPage, bCb ){
 	if( arguments.length!=3 ){bCb("Function argument missed!");return;} 	
 	if( typeof(arguments[1])!="number" && typeof(arguments[2])!="function" ){bCb("Argument type error!");return;}
-	request('https://www.bing.com/search?q=' + arguments[0], function (error, response, html) {
-	process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
-	console.log(error);
- 	if (!error && response.statusCode == 200) { bCb(html.match(/http(s|)\:\/\/(www.|)(\<strong\>|)elwiki\.net\/(.*?)(?=\")/gi)); }
+	var url = String('https://elwiki.net/wiki/index.php?title=Special:Search&search=-+Rose');
+	request(url, function (error, response, body) {	
+	if(error){return;}	
+	request(response.request.uri.href, function (a, b, c) {
+	console.log(c);	
+	});
 	});
 }
 
@@ -34,8 +36,7 @@ exports.elspoiler = function ( bQuery, bPage, bCb ){
 	if(error){return;}	
 	request(response.request.uri.href, function (a, b, c) {
 	var results_table = c.match(/showthread(.*?)(?=\;s)/gi);
-	var results_table = results_table.filter(function(k){ if(!k.toLowerCase().match(/(c|v|t)\-gt/g)){return k;} });
-	console.log(results_table);	
+	var results_table = results_table.filter(function(k){ if(!k.toLowerCase().match(/(c|v|t)\-gt/g)){return k;} });	
 	if(results_table.length > 0){bCb(results_table);}	
 	});
 	});

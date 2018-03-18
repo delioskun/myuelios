26840c82611e6c9e331c588c2bbae2022413dd2b
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-18 00:04:29 -0300

    Update myu.js

--- myu.js
+++ myu.js
@@ -152,7 +152,8 @@ myu.on('message', message => {
 			case "elspoiler":
 			usersearchview = "Elspoiler :last level up"; 
 			js.forum(encodeURIComponent(usersearchview),1,function(response){
-			if (!r_f){ var result_tab = response.filter(function(n){return n.match(/elsword\//g)})[0]}; r_f = true;
+			console.log(response);	
+			if (!r_f && response != null){ var result_tab = response.filter(function(n){return n.match(/elsword\//g)})[0]}; r_f = true;
 			if(result_tab != undefined){
 				isgd.shorten(`${result_tab}`, function(res) { message.reply(`***Elspoiler desta semana! Confira:***\n${res}`) });
 			}			

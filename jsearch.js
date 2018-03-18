c9e2e16df3e2d4916e6bb0291c918feef9b45ab4
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-18 17:19:35 -0300

    Update jsearch.js

--- jsearch.js
+++ jsearch.js
@@ -4,7 +4,7 @@ var cheerio = require('cheerio');
 exports.elwiki = function ( bQuery, bPage, bCb ){
 	if( arguments.length!=3 ){bCb("Function argument missed!");return;} 	
 	if( typeof(arguments[1])!="number" && typeof(arguments[2])!="function" ){bCb("Argument type error!");return;}
-	var url = String('https://www.bing.com/search?q=' + encodeURIComponent(bQuery + 'elwiki'));
+	var url = String('https://www.bing.com/search?q=' + encodeURIComponent(bQuery + ' elwiki'));
 	request(url, function (error, response, body) {
 	console.log(body);
 	});

46a3c69902b5703e99b0ed13239315a273e42d0c
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-18 15:07:40 -0300

    Update jsearch.js

--- jsearch.js
+++ jsearch.js
@@ -19,8 +19,8 @@ exports.forum = function ( bQuery, bPage, bCb ){
 	if(error){return;}	
 	request(response.request.uri.href, function (a, b, c) {
 	var results_table = c.match(/showthread(.*?)(?=\;s)/gi);
-	console.log(results_table.filter(function(k){ if(!k.toLowerCase().match(/(c|v)\-gt/g)){return k;} }));	
-	if(results_table.length > 0){bCb(results_table.filter(function(k){ if(!k.toLowerCase().match(/(c|v)\-gt/g)){return k;} }));}	
+	var results_table = results_table.filter(function(k){ if(!k.toLowerCase().match(/(c|v|t)\-gt/g)){return k;} });	
+	if(results_table.length > 0){bCb(results_table);}	
 	});
 	});	
 }

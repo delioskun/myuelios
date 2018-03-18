61f30fe0fe6743985ba20f55e37da463cd6ee974
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-18 00:10:56 -0300

    Update jsearch.js

--- jsearch.js
+++ jsearch.js
@@ -33,5 +33,21 @@ exports.forum = function ( bQuery, bPage, bCb ){
 	}
 }
 
+exports.elspoiler = function ( bQuery, bPage, bCb ){
+	if( arguments.length===3 ){ 	
+		if( typeof(arguments[1])=="number" && typeof(arguments[2])=="function" ){
+			request('https://www.bing.com/search?q=' + arguments[0], function (error, response, html) {
+			process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+			console.log(html);
+ 		 if (!error && response.statusCode == 200) { bCb(html.match(/http(s|)\:\/\/(www.|)(\<strong\>|)sites\.levelupgames\.com\.br\/(.*?)(?=\")/gi)); }
+	});
+		}else{
+			bCb("Argument type error!");
+		}
+	}else{
+		bCb("Function argument missed!");
+	}
+}
+
 
 

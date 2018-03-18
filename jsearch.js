27c1d8fc32e53e6c2675a457d661557c038a2439
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-18 18:09:52 -0300

    Update jsearch.js

--- jsearch.js
+++ jsearch.js
@@ -8,7 +8,7 @@ exports.elwiki = function ( bQuery, bPage, bCb ){
 	var url = String('https://elwiki.net/wiki/index.php?search=' + encodeURIComponent(bQuery));
 	request(url, function (error, response, body) {
 	if(error){return;}
-	console.log(response.request.uri.href);	
+	if(!response.request.uri.href.match(/\?search/gi)){bCb(['response.request.uri.href']);return;}
 	});
 }
 

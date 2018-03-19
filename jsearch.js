3cbd75d0a53089b313e81ee5125625aed69cd301
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-19 04:36:55 -0300

    Update jsearch.js

--- jsearch.js
+++ jsearch.js
@@ -16,9 +16,8 @@ exports.forum = function ( bQuery, bPage, bCb ){
 	if( arguments.length!=3 ){bCb("Function argument missed!");return;} 	
 	if( typeof(arguments[1])!="number" && typeof(arguments[2])!="function" ){bCb("Argument type error!");return;}
 	  request('http://sites.levelupgames.com.br/forum/elsword/showthread.php?39107-%CDndice-de-Guias', function (error, response, body) {
-    	  var re = new RegExp('showthread(.*?)' + encodeURI(bQuery.replace(/\s/gi,"-")) + '((.*?)|)(?=\\")','g');
-	  console.log(body.match(re));	  
-    	  if(body.match(re) != undefined){bCb(body.match(re));return;}	  
+    	  var re = new RegExp('showthread(.*?)' + encodeURI(bQuery.replace(/\s/gi,"-")) + '((.*?)|)(?=\\")','g');  
+    	  if(body.match(re) != undefined){console.log(body.match(re)); bCb(body.match(re));return;}	  
   	  });
 	if(r_s(bQuery) == "Índice de Guias"){bCb(['showthread.php?39107-%CDndice-de-Guias']);return;}
 	var url = String('http://sites.levelupgames.com.br/forum/elsword/search.php?do=process&query=' + r_s(bQuery) + '&titleonly=1');

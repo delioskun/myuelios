05e50aab222f5c7a9e326ce0174d2f4c6f420d6b
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-19 03:24:19 -0300

    Update jsearch.js

--- jsearch.js
+++ jsearch.js
@@ -58,7 +58,7 @@ while(opl < 3);
 
 function indice_files(content){ 
   request('http://sites.levelupgames.com.br/forum/elsword/showthread.php?39107-%CDndice-de-Guias', function (error, response, body) {
-    var re = new RegExp('/' + content + '/','g');
+    var re = new RegExp(encodeURI(content.replace(/\s/gi,"-")),'g');
     console.log('teste:' + re);
   });
   return ["showthread.php?39107-%CDndice-de-Guias"];

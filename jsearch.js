efc3af799d3f72558b2bb10c8f565fef61fdf008
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-19 04:03:58 -0300

    Update jsearch.js

--- jsearch.js
+++ jsearch.js
@@ -15,6 +15,7 @@ exports.elwiki = function ( bQuery, bPage, bCb ){
 exports.forum = function ( bQuery, bPage, bCb ){
 	if( arguments.length!=3 ){bCb("Function argument missed!");return;} 	
 	if( typeof(arguments[1])!="number" && typeof(arguments[2])!="function" ){bCb("Argument type error!");return;}
+	console.log(indice_files(bQuery));
 	if(indice_files(bQuery) || r_s(bQuery) == "Índice de Guias"){bCb(indice_files(bQuery));return;}
 	var url = String('http://sites.levelupgames.com.br/forum/elsword/search.php?do=process&query=' + r_s(bQuery) + '&titleonly=1');
 	request(url, function (error, response, body) {	
@@ -59,6 +60,6 @@ while(opl < 3);
 function indice_files(content){ 
   request('http://sites.levelupgames.com.br/forum/elsword/showthread.php?39107-%CDndice-de-Guias', function (error, response, body) {
     var re = new RegExp('showthread(.*?)' + encodeURI(content.replace(/\s/gi,"-")) + '((.*?)|)(?=\\")','g');	  
-    if(body.match(re)){ console.log(["me",body.match(/showthread(.*?)Rose((.*?)|)(?=\")/gi)]);return body.match(re); }else{return ["showthread.php?39107-%CDndice-de-Guias"];}
+    return(body.match(re) ? body.match(re) : ["showthread.php?39107-%CDndice-de-Guias"]);
   });
 }  

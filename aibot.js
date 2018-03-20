c10f74527784b361e8124d11935ee85882b04b86
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-20 19:40:34 -0300

    Update aibot.js

--- aibot.js
+++ aibot.js
@@ -1,3 +1,3 @@
 exports.message = function ( content,author ){
-  return "Eu não entendi muito bem o que quis dizer. Pode repetir?";
+  return "Eu não entendi muito bem o que quis dizer " + author + ". Pode repetir?";
 }

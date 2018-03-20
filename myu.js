a05ce27409f64c98bf07beb79bec20ae21bef22b
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-20 19:29:04 -0300

    Update myu.js

--- myu.js
+++ myu.js
@@ -73,7 +73,7 @@ myu.on('message', message => {
 		]
 		message.channel.send(replies[Math.floor((Math.random() * 4))]);
 	        }else{
-		message.channel.send(ai_bot.message(args.join(" ")));	
+		message.channel.send(ai_bot.message(args.join(" ")),message.member);	
 		}
 		}else{			
 		if(["forum","elwiki","elspoiler"].includes(command.toLowerCase()) && (!Object.keys(timeout_users).includes('user_' + message.author.id) ||  message.member.permissions.has('ADMINISTRATOR'))){

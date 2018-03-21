1e69ae641bc373321dd7f69600bfd8972917f743
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-21 03:17:08 -0300

    Update aibot.js

--- aibot.js
+++ aibot.js
@@ -4,18 +4,19 @@ return { 'cache-control': conn_ai[0], 'x-apikey': conn_ai[1], 'content-type': co
 }
 
 exports.message = function ( content,author ){
+  var replie = "";	
   var options = { method: 'GET', url: process.env.BOT_AIDBPATH, headers: connect_db() };
   var request = require("request");
 		request(options, function (error, response, body) {
 		if (error) throw new Error(error);
-		var replie = "";var size = eval(body).length - 1;		
+		var size = eval(body).length - 1;		
 		for(i=0;i < size;i++){
 		var re = new RegExp(question.pergunta,'gi');	
 	  	if(content.match(re)){
 		var args = eval(question.respostas);
-		if(question.cond && question.cond.length > 0){eval(question.cond); var replie = eval(sek);break;}    
-	  	}	
+		if(question.cond && question.cond.length > 0){eval(question.cond); replie = eval(sek);break;}}	
 		}
 		});
+	        console.log(replie);
   		return (replie.length > 0 ? replie : "No momento não estou programada para responder a isto " + author.username + ". x3");
 }

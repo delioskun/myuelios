54e2302e68f9a6cd280af0fc974628dd351ec739
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-21 02:30:50 -0300

    Update aibot.js

--- aibot.js
+++ aibot.js
@@ -1,5 +1,3 @@
-var mysql = require('mysql');
-
 function connect_db(){
 let conn_ai = eval(process.env.BOT_AIDB);
 return { 'cache-control': conn_ai[0], 'x-apikey': conn_ai[1], 'content-type': conn_ai[2] };

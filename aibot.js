016e315eff9bb4741e76d33f9c8fb9000b4a93f5
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-21 18:00:05 -0300

    Update aibot.js

--- aibot.js
+++ aibot.js
@@ -14,10 +14,10 @@ exports.message = function ( content,author,bcb ){
 		var args = eval(eval(body)[i].respostas);	
 		if(eval(body)[i].site_auxiliar != "nenhum"){
 		var url = String(eval(body)[i].site_auxiliar);
-		request(url, function (err, resp, bd) {	
-		console.log(bd);	
+		request(url, function (err, resp, bd) {		
 		if(error){bcb("Aconteceu algo de errado nos meus sensores. Tente novamente.");return;}
 		request(resp.request.uri.href, function (a, b, c) {
+		console.log(c);	
 		if(eval(body)[i].cond && eval(body)[i].cond.length > 0){eval(eval(body)[i].cond); bcb(eval(sek)); return;}	
 		});	
 		});	

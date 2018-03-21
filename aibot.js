ea9f5b49229fd2a5bcc4dd5c80921a5c72344f0c
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-21 03:39:40 -0300

    Update aibot.js

--- aibot.js
+++ aibot.js
@@ -3,19 +3,19 @@ let conn_ai = eval(process.env.BOT_AIDB);
 return { 'cache-control': conn_ai[0], 'x-apikey': conn_ai[1], 'content-type': conn_ai[2] };
 }
 
-exports.message = function ( content,author ){
-  var replie_dft = true;	
+exports.message = function ( content,author ){	
   var options = { method: 'GET', url: process.env.BOT_AIDBPATH, headers: connect_db() };
   var request = require("request");
 		request(options, function (error, response, body) {
 		if (error) throw new Error(error);
-		var size = eval(body).length;	
+		var size = eval(body).length;
+		var replie_dft = true;	
 		for(i=0;i < size;i++){	
 		var re = new RegExp(eval(body)[i].pergunta,'gi');	
 	  	if(content.match(re)){
 		var args = eval(eval(body)[i].respostas);	
 		if(eval(body)[i].cond && eval(body)[i].cond.length > 0){eval(eval(body)[i].cond); replie_dft = false; return eval(sek); break;}}
 		}
+		if(replie_dft){ return "No momento não estou programada para responder a isto " + author.username + ". x3"; }	
 		});
-	        if(replie_dft){ return "No momento não estou programada para responder a isto " + author.username + ". x3"; }
 }

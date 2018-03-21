bc8b1778b40a574a13110c2555cec899d427a376
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-21 01:49:25 -0300

    Update aibot.js

--- aibot.js
+++ aibot.js
@@ -1,3 +1,19 @@
+var mysql = require('mysql');
+
+function connect_db(){
+var con = mysql.createConnection({
+  host: process.env.BOT_AIDB[0],
+  user: process.env.BOT_AIDB[1],
+  password: process.env.BOT_AIDB[2]
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});  
+}
+
 exports.message = function ( content,author ){
+  connect_db();
   return "No momento não estou programada para responder a isto " + author.username + ". x3";
 }

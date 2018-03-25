3017a66ed4d0c8449e443da6e2cfe66edd253c38
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-25 19:59:50 -0300

    Update aibot.js

--- aibot.js
+++ aibot.js
@@ -5,7 +5,7 @@ let conn_ai = eval(process.env.BOT_AIDB);
 return { 'cache-control': conn_ai[0], 'x-apikey': conn_ai[1], 'content-type': conn_ai[2] };
 }
 
-exports.message = function ( contt,author,bcb ){	
+exports.message = function ( contt,mentions,author,bcb ){	
   var content = contt.replace(/^@\d+\>/i,"");
   var options = { method: 'GET', url: process.env.BOT_AIDBPATH, headers: connect_db() };
   var request = require("request");var i = 0;

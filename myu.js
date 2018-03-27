4e4f87c462be23a38a15ee2491e9d0d96cd5a903
delioskun <37356195+delioskun@users.noreply.github.com>
2018-03-26 23:14:41 -0300

    Update myu.js

--- myu.js
+++ myu.js
@@ -43,7 +43,10 @@ myu.login(process.env.BOT_TOKEN);
 myu.on('ready', () => { myu.user.setActivity('Elesis. O jogo de ação do momento!');console.log('Driver on! Please!'); })
 
 myu.on('message', message => {
-		console.log(message.guild.name);
+		if(message.guild.name == 'Laboratório da Noratinha'){
+		if(!data_montly[message.guild.id]){data_montly[message.guild.id] = { name: message.guild.name; }}
+		console.log(data_montly[message.guild.id]);	
+		}
 		if(message.cleanContent.startsWith('@Myu')){
 		const authorname = message.author;
 		const myu_mentions = message.mentions;	

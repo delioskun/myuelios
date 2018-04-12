cb287f9b6f16f5d92c4108cf92ed3737c2701acd
delioskun <37356195+delioskun@users.noreply.github.com>
2018-04-12 18:05:02 -0300

    Update myu.js

--- myu.js
+++ myu.js
@@ -13,8 +13,8 @@ var timeout_users = [];
 var data_montly = [];
 var myu_online = false;
 var time = new Date();
-var day = time.getDate();var month = time.getMonth(); var year = time.getFullYear();
-var hra = time.getHours() - 2;var min = time.getMinutes(); var sec = time.getSeconds();
+var day = time.getDate();var month = time.getMonth() + 1; var year = time.getFullYear();
+var hra = time.getHours() - 3;var min = time.getMinutes(); var sec = time.getSeconds();
 	  
 /******************************************************/	  
 	  
@@ -183,8 +183,8 @@ myu.on('message', message => {
 		 request(options, function (error, response, body) {
 		 if (error) throw new Error(error);
 		 if(data_exists(body,message.guild.id,'report_channel')){
-		 var quote = "```"; var fulldate = day + "/" + month + "/" + year; var fullhour = hra + ":" + min + ":" + sec; 	
-		 message.guild.channels.find("name", eval(body)[i].server_value).send(`${quote}**Denuncia de ${userid}**\n**Denunciado:** ${userwarn}\n**Data e hora da denuncia:** ${fulldate} às ${fullhour}\n**Canal do ocorrido:** ${message.channel.name}\n**Causa:** ${warningtext}${quote}` );
+		  var fulldate = day + "/" + month + "/" + year; var fullhour = hra + ":" + min + ":" + sec; 	
+		 message.guild.channels.find("name", eval(body)[i].server_value).send(```Denúncia de ${userid}${quote}Markdown\n================\n[Denunciado]()\n[Canal](message.channel.name)\n[Data e Hora do Ocorrido](${fulldate} às ${fullhour})\n[Causa](${warningtext})\n${quote}```);
 		 }});
 		 message.delete(0, console.log(''));
 		 break;

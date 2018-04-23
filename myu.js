1669d02ad55ce44bea00c72353a1a99320c80625
delioskun <37356195+delioskun@users.noreply.github.com>
2018-04-22 22:36:41 -0300

    Update myu.js

--- myu.js
+++ myu.js
@@ -46,6 +46,9 @@ myu.login(process.env.BOT_TOKEN);
 myu.on('ready', () => { myu.user.setActivity('Elesis. O jogo de ação do momento!');console.log('Driver on! Please!'); })
 
 myu.on('message', message => {
+		if(message.author == "Drabaki" && message.content == "OUSA MESMO ME ENFRENTAR? DESAPAREÇA!!!"){
+			message.channel.send("```Markdown #Cuidado! Drabaki irá atacar! Defenda-se!```");
+		}
 		if(message.cleanContent.startsWith('@Myu')){
 		const authorname = message.author;
 		const myu_mentions = message.mentions;	
